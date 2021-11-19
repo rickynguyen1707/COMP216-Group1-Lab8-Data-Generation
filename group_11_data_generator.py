29f4e6044ca27c4ac93477e0322f0cec7b45e242
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2021-11-18 19:59:43 -0500

    Part 2 added

--- group_11_data_generator.py
+++ group_11_data_generator.py
@@ -1,10 +1,22 @@
 import random
+import matplotlib
+import matplotlib.pyplot as plt
 
 class DataGenerator:
     def __init__(self,numOfValues = 10, rangeStart = 0, rangeEnd = 1):
         self.numOfValues = numOfValues
         self.rangeStart = rangeStart
         self.rangeEnd = rangeEnd
+
+    def __randomNormalizer(self):
+        randomNum = random.gauss(self.rangeStart,self.rangeEnd)
+        return randomNum
+
+    
+
+
+#data = DataGenerator()
+#data.randomNormalizer()
 
 #testing code for class
 #     def generate(self):
