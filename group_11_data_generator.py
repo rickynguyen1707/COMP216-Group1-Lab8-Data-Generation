0df7c64d7475959604bb8acefb15a898a2066da1
amneshia <amirshayan.armaghan@gmail.com>
2021-11-20 02:48:08 -0500

    Update group_11_data_generator.py

--- group_11_data_generator.py
+++ group_11_data_generator.py
@@ -1,20 +1,29 @@
 import random
-import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 class DataGenerator:
-    def __init__(self,numOfValues = 10, rangeStart = 0, rangeEnd = 1):
-        self.numOfValues = numOfValues
-        self.rangeStart = rangeStart
-        self.rangeEnd = rangeEnd
+    def __init__(self, num_vals=10, range_start=0, range_end=1):
+        self.num_vals = num_vals
+        self.range_start = range_start
+        self.range_end = range_end
 
-    def __randomNormalizer(self):
-        randomNum = random.gauss(self.rangeStart,self.rangeEnd)
-        return randomNum
+    def __gen_points(self):
+        return np.array([random.random() for _ in range(self.num_vals)])
 
-    def getDataInRange(self):
-        return (self.rangeEnd-self.rangeStart)*(self.__randomNormalizer())+(self.rangeStart)
+    def data_in_range(self):
+        return (self.range_end - self.range_start) * self.__gen_points() + self.range_start
 
-#test code for class
-#x = DataGenerator(5,0,10)
-#print(x.getDataInRange())
+    def plot(self, points):
+        plt.plot(points, color='g')
+        plt.xlabel("Day of Month (Nov)")
+        plt.ylabel("Price ($)")
+        plt.title("Stock Prices")
+        plt.show()
+
+
+if __name__ == '__main__':
+    gen = DataGenerator(30, 112, 125)
+    data = gen.data_in_range()
+    gen.plot(data)
